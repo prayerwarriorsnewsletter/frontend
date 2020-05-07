3ee2947d8904548012ff9dbb638c2dd8ac46b4a8
Hannah Tuttle <hannahtuttle28@gmail.com>
2020-05-07 07:27:19 -0700

    added new prayer request form, still needs style and hook up to server

--- client/src/components/PrayerList.js
+++ client/src/components/PrayerList.js
@@ -1,11 +1,13 @@
 import React from "react";
 import PrayerCard from "./PrayerCard";
+import NewRequest from './NewRequest.js'
 
 const PrayerList = (props) => {
   const { prayerData } = props;
   console.log("prayerData", prayerData);
   return (
     <div>
+      {/* <NewRequest/> */}
       {prayerData &&
         prayerData.map((prayer, index) => (
           <PrayerCard key={index} prayer={prayer} />
